f142f5a7fd4c59f15ad66a1e01a5311043c54151
hyohee0613 <gygml9@gmail.com>
2025-04-17 15:08:25 +0900

    사칙연산 수행까지 완료

--- src/lv2/Calculator.java
+++ src/lv2/Calculator.java
@@ -1,4 +1,39 @@
 package lv2;
 
 public class Calculator {
+
+    //사칙연산을 수행 후, 결과값 반환 메서드 구현 & 연산 결과를 저장하는 컬렉션 타입 필드를 가진 Calculator 클래스
+
+    //속성
+    private int a;
+    private int b;
+
+    //생성자
+    public Calculator (int a, int b){
+        this.a = a;
+        this.b = b;
+    }
+
+    //기능
+     int sum (){
+        int result = a + b;
+        return result;
+    }
+
+    int sub () {
+        int result = a - b;
+        return result;
+    }
+
+    int mul () {
+        int result = a * b;
+        return result;
+    }
+
+    double div () {
+        double result = (double) a / (double) b;
+        return result;
+    }
+
+
 }
--- src/lv2/Main.java
+++ src/lv2/Main.java
@@ -1,7 +1,32 @@
 package lv2;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("첫번째 숫자를 입력해주세요.: ");
+        int a = scan.nextInt();
+
+        System.out.println("두번째 숫자를 입력해주세요.: ");
+        int b = scan.nextInt();
+
+
+        Calculator calculator = new Calculator (a, b);
+
+        int sum = calculator.sum();
+        System.out.println("계산 결과: " + sum);
+
+        int sub = calculator.sub();
+        System.out.println("계산 결과: " + sub);
+
+        int mul = calculator.mul();
+        System.out.println("계산 결과: " + mul);
+
+        double div = calculator.div();
+        System.out.println("계산 결과: " + div);
 
     }
 }
