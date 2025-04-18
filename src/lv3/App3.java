110a29729da2f0cb292da0231fdb4eaf24a1b41a
hyohee0613 <gygml9@gmail.com>
2025-04-18 19:18:01 +0900

    입력받기 완료

--- src/lv3/App3.java
+++ src/lv3/App3.java
@@ -1,4 +1,82 @@
 package lv3;
 
+import lv3.Calculator3;
+import java.util.Scanner;
+
 public class App3 {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        Calculator3 calculator = new Calculator3();
+
+        //숫자 a, b 입력받기
+
+            while (true) {
+                System.out.println("첫번째 숫자를 입력하세요: ");
+                if (scan.hasNextInt()) {
+                    int a = scan.nextInt();
+                    try {
+                        calculator.setA(a);
+                        break;
+                    }
+                    catch (Exception e) {
+                        System.out.println(e.getMessage());
+                    }
+                } else {
+                    System.out.println("숫자로 다시 입력해주세요");
+                    scan.nextLine();    // 입력 버퍼 제거
+                }
+            }
+
+            while (true) {
+                System.out.println("두번째 숫자를 입력하세요: ");
+                if (scan.hasNextInt()) {
+                    int b = scan.nextInt();
+                    try {
+                        calculator.setB(b);
+                        break;
+                    } catch (Exception e) {
+                        System.out.println(e.getMessage());
+                    }
+                } else {
+                    System.out.println("숫자로 다시 입력해주세요");
+                    scan.nextLine();    // 입력 버퍼 제거
+                }
+            }
+
+
+        //사칙연산자 c 입력받기
+
+        while (true) {
+            System.out.println("사칙연산 기호를 입력해주세요. (+, -, *, /)");
+            String c = scan.nextLine();
+
+            try {
+                calculator.setC(c);
+                break;
+
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+            scan.nextLine();    // 입력 버퍼 제거
+        }
+
+
+        //연산하고 출력하기
+
+
+
+
+        //계산을 더 진행할지 끝낼지 확인
+
+
+
+
+
+
+
+
+
+
+    }
 }
--- src/lv3/Calculator3.java
+++ src/lv3/Calculator3.java
@@ -1,4 +1,78 @@
 package lv3;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Calculator3 {
+
+    //속성
+    private int a;
+    private int b;
+    private String c;
+    private List<Double> resultAB = new ArrayList<>();
+
+
+    //생성자
+    public Calculator3() {}
+
+
+    //기능
+    public double calculate() {
+        this.c.charAt(0);
+        double result = 0;
+        switch (c.charAt(0)) {
+            case '+':
+                result = a + b;
+                break;
+            case '-':
+                result = a - b;
+                break;
+            case '*':
+                result = a * b;
+                break;
+            case '/':
+                if (b == 0) {
+                    throw new ArithmeticException("두번째 입력한 숫자가 0인 경우 나눗셈할 수 없습니다.");
+                }
+                result = (double) a / b;
+                break;
+
+        }
+        resultAB.add(result);
+        return result;
+    }
+
+
+    //게터
+      public List<Double> getResultAB() {
+          return resultAB;
+      }
+
+      //세터: 값 저장 전 검사
+      public void setA(int a) {
+          if (a < 0) {
+              throw new IllegalArgumentException("0을 포함한 양의 정수로 다시 입력해주세요");
+          }
+          this.a = a;
+      }
+
+      public void setB(int b) {
+          if (b < 0) {
+              throw new IllegalArgumentException("0을 포함한 양의 정수로 다시 입력해주세요");
+          }
+          this.b = b;
+      }
+
+      public void setC(String c) {
+        if ((c.length() != 1) || !"+-*/".contains(c)){
+              throw new IllegalArgumentException("+, -, *, / 중 한 개만 입력해주세요.");
+          }
+          this.c = c;
+      }
+
+    // resultAB 리스트에 저장된 연산 결과들 중 가장 먼저 저장된 데이터를 삭제 (List에 있는 기본 메서드)
+    public void removeResult() {
+        resultAB.remove(0);
+    }
+
 }
